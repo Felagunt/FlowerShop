59295a4e46241de4f85a86e3c95b34513fa6f976
Mezin Ilya <felagunt@live.ru>
2016-05-19 18:52:30 +0300

    merde

--- FlowerShop/Program.cs
+++ FlowerShop/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace FlowerShop
 {
@@ -10,18 +11,20 @@ namespace FlowerShop
     {
         static void Main(string[] args)
         {
-            Bouquet[] bouquet = new Bouquet[2];
+            Bouquet[] bouquet = new Bouquet[3];
 
-            bouquet[0] = new RosesStandard();
-            bouquet[1] = new PetuniasStandard();
-
+            bouquet[0] = new RosesStandard(2);
+            bouquet[1] = new PetuniasStandard(4);
+            bouquet[2] = new Custom(3);
             //display participants of bouguet
             foreach(Bouquet participants in bouquet)
             {
-                Console.WriteLine("\n" + bouquet.GetType().Name + "--");
-                foreach(Flower flower in participants.Flowers)
+                Console.WriteLine("\n" + bouquet.GetType().Name + "--"+
+                    " Price:"+Flower.price);
+                foreach (Flower flower in participants.Flowers)
                 {
-                    Console.WriteLine(" " + flower.GetType().Name);
+                    Console.WriteLine(" " + flower.GetType().Name +
+                        " Price:" + participants.Coast);
                 }
             }
 
@@ -32,37 +35,42 @@ namespace FlowerShop
     
     abstract class Flower
     {
-       /* public int count { get; set; }
-
-        public Flower()
-        {
-            
-        }*/
+      public static int price { get; set; }
     }
     class Rose:Flower
     {
         public Rose()
         {
-            Console.WriteLine("Rose adding");
+            price = 200;
         }
     }
     class Peony:Flower
     {
-
+        public Peony()
+        {
+            price = 100;
+        }
     }
 
     class Petunia:Flower
     {
-
+        public Petunia()
+        {
+            price = 132;
+        }
     }
-
+    
     //
     abstract class Bouquet
     {
         private List<Flower> _flowers = new List<Flower>();
-
-        public Bouquet()
+        public int Coast { get; set; }
+        public int Count { get; set; }
+        public Bouquet(int c)
         {
+            Count = c;
+            Coast = Flower.price * Count;
+           
             this.CreateBouquet();
         }
 
@@ -70,22 +78,42 @@ namespace FlowerShop
         {
             get { return _flowers; }
         }
-
+        
         public abstract void CreateBouquet();
     }
 
+    
     class RosesStandard:Bouquet
     {
+        public RosesStandard(int c) : base(c)
+        { }
         public override void CreateBouquet()
         {
             Flowers.Add(new Rose());
         }
+
+       
     }
     class PetuniasStandard:Bouquet
     {
+        public PetuniasStandard(int c):base(c)
+        { }
         public override void CreateBouquet()
         {
             Flowers.Add(new Petunia());
         }
+        
+    }
+
+    class Custom : Bouquet
+    {
+        public Custom(int c):base(c)
+        { }
+        public override void CreateBouquet()
+        {
+            Flowers.Add(new Rose());
+            Flowers.Add(new Peony());
+        }
+        
     }
 }
